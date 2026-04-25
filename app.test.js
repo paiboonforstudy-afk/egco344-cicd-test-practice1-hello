5227289b2d0e00dab14b9d6474f8a2340ab9de63
unknown <paiboonforstudy@gmail.com>
2026-04-25 16:06:48 +0700

    Fix: restore correct test case

--- app.test.js
+++ app.test.js
@@ -5,7 +5,7 @@ describe('GET /', () => {
   it('should return 200 OK and Hello World message', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toEqual(200);
-    expect(res.text).toContain('Goodbye World');
+    expect(res.text).toContain('Hello World');
   });
 });
 
